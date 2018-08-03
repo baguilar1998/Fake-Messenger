32de70bb8e7aa5a1f2dc8ba642264b2b49e8686a
Brian Kenji Aguilar <anjacular@gmail.com>
2018-08-02 20:48:50 -0400

    Landing Page: Added in the login component and working on the UI for it

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './landing-page/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
