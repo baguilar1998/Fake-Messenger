7ba8d53fd24e90ea5bbbe52fa4cf6f7e4f1495ff
Brian Kenji Aguilar <anjacular@gmail.com>
2018-08-03 20:05:49 -0400

    Login Component: Setting up logic for sign up

--- front-end/src/app/landing-page/login/login.component.ts
+++ front-end/src/app/landing-page/login/login.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private signUp: boolean;
+  constructor() {
+    this.signUp = false;
+  }
 
   ngOnInit() {
+  }
+
+  // Changes the sign up variable depending on what button the user clicks
+  setSignUp(): void {
+    this.signUp = !this.signUp;
+  }
+
+  // Checks to see if the user is on the sign up view
+  checkSignUp(): boolean {
+    return this.signUp;
   }
 
 }
