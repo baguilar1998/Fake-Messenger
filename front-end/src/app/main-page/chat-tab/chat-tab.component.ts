d690aa66ef262866f0ff9ddd05174581778df474
Brian Kenji Aguilar <anjacular@gmail.com>
2018-09-11 18:48:58 -0400

    Chat-Tab: Sends in static messages to the screen and displays it dynamically. Thumbs up picture is sent when the user clicks the thumbs up icon

--- front-end/src/app/main-page/chat-tab/chat-tab.component.ts
+++ front-end/src/app/main-page/chat-tab/chat-tab.component.ts
@@ -10,6 +10,24 @@ import { FriendService } from '../../services/friend.service';
 export class ChatTabComponent implements OnInit, OnDestroy {
 
   selectedFriend = this.friendService.getSelectedFriend();
+  messages = [
+    {
+      message: 'Hello friend !',
+      user: 0
+    },
+    {
+      message: 'Hey There !',
+      user: 1
+    },
+    {
+      message: 'How are you doing ?',
+      user: 1
+    },
+    {
+      message: 'Im doing well. How about you ?',
+      user: 0
+    }
+  ];
   subscription;
   constructor(private userService: UserService, private friendService: FriendService) {
     this.subscription = friendService.selectedFriendChange.subscribe((data) => this.selectedFriend = data );
@@ -22,4 +40,29 @@ export class ChatTabComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
   }
 
+  /**
+   *  Sends a message
+   * @param event message that user entered
+   */
+  sendMessage(event) {
+    let message;
+    if (event === '(y)') {
+      message = {
+        message: '(y)',
+        user: 0
+      };
+    } else {
+      if (event.target.value === '') { return; }
+      message = {
+       message: event.target.value,
+       user: 0
+     };
+    }
+    this.messages.push(message);
+    (document.getElementById('res') as HTMLInputElement).value = '';
+  }
+
+  thumbsUp() {
+    this.sendMessage('(y)');
+  }
 }
